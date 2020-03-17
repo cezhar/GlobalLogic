b965297548dc2fd9457a49571a85ab395f062f9a
Cezhar Arévalo <cezhar.arevalo@ionix.cl>
2020-03-17 20:46:33 -0300

    first commit

---
+++ Test/model/Song.swift
@@ -0,0 +1,42 @@
+//
+//  Song.swift
+//  Test
+//
+//  Created by Cezhar Arévalo on 17-03-20.
+//  Copyright © 2020 Cezhar Arévalo. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Codable {
+    let wrapperType, kind: String
+    let artistID, collectionID, trackID: Int
+    let artistName, collectionName, trackName, collectionCensoredName: String
+    let trackCensoredName: String
+    let artistViewURL, collectionViewURL, trackViewURL: String
+    let previewURL: String
+    let artworkUrl30, artworkUrl60, artworkUrl100: String
+    let collectionPrice, trackPrice, collectionHDPrice, trackHDPrice: Double
+    let releaseDate: Date
+    let collectionExplicitness, trackExplicitness: String
+    let discCount, discNumber, trackCount, trackNumber: Int
+    let trackTimeMillis: Int
+    let country, currency, primaryGenreName, contentAdvisoryRating: String
+    let shortDescription, longDescription: String
+
+    enum CodingKeys: String, CodingKey {
+        case wrapperType, kind
+        case artistID = "artistId"
+        case collectionID = "collectionId"
+        case trackID = "trackId"
+        case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
+        case artistViewURL = "artistViewUrl"
+        case collectionViewURL = "collectionViewUrl"
+        case trackViewURL = "trackViewUrl"
+        case previewURL = "previewUrl"
+        case artworkUrl30, artworkUrl60, artworkUrl100, collectionPrice, trackPrice
+        case collectionHDPrice = "collectionHdPrice"
+        case trackHDPrice = "trackHdPrice"
+        case releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, contentAdvisoryRating, shortDescription, longDescription
+    }
+}
